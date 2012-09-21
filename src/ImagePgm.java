a43586abf5bbd015df36810860d7eaac04dc3007
bb44 <brice.Le-dain@eleve.ec-nantes.fr>
2012-09-21 12:39:23 +0200

    essai

---
+++ src/ImagePgm.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+public class ImagePgm {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Image");
+//String path= "C:\Users\bb\Downloads\peppers512x512.pgm";
+
+//String chaine="";
+Vector<Integer> carac=new Vector();
+String fichier ="lena512x512.pgm";
+
+//lecture du fichier texte	
+try{
+	InputStream ips=new FileInputStream(fichier); 
+	InputStreamReader ipsr=new InputStreamReader(ips);
+	BufferedReader br=new BufferedReader(ipsr);
+	String ligne;
+	int numligne=0;
+	while ((ligne=br.readLine())!=null){numligne++;
+		System.out.println(ligne);
+		if(numligne>3){
+		carac.addElement(Integer.valueOf(ligne));}
+	}
+	br.close(); 
+	for(int j=0; j< carac.size();j++){
+	System.out.println(carac.get(j));
+	}
+}		
+catch (Exception e){
+	System.out.println(e.toString());
+}
+Vector<Integer> histo= new Vector(); 
+for(int a=0;a<256;a++){
+	histo.addElement(0);
+	for(int b=0;b<carac.size();b++){
+		if(carac.get(b)==a){histo.setElementAt(histo.get(a)+1,a);}
+	}
+}
+int r;
+// renvoie l'histogramme
+for(int a=0;a<256;a++){
+		r=histo.get(a);
+		System.out.println("Nombre de pixels de valeur" +a+": "+r);
+}
+/*
+//création ou ajout dans le fichier texte
+try {
+	FileWriter fw = new FileWriter (fichier);
+	BufferedWriter bw = new BufferedWriter (fw);
+	PrintWriter fichierSortie = new PrintWriter (bw); 
+		fichierSortie.println (chaine+"\n test de lecture et écriture !!"); 
+	fichierSortie.close();
+	System.out.println("Le fichier " + fichier + " a été créé!"); 
+}
+catch (Exception e){
+	System.out.println(e.toString());
+}	*/	
+	}
+}
