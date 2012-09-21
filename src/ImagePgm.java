5c0a55b36fd519de819a3b6d0e4731cd6239dd13
bb44 <brice.le-dain@eleves.ec-nantes.fr>
2012-09-21 14:26:21 +0200

    fin

--- src/ImagePgm.java
+++ src/ImagePgm.java
@@ -13,16 +13,22 @@ System.out.println("Image");
 //String chaine="";
 Vector<Integer> carac=new Vector();
 String fichier ="lena512x512.pgm";
-
+String l1="";
+String l2="";
+String l3="";
 //lecture du fichier texte	
 try{
 	InputStream ips=new FileInputStream(fichier); 
 	InputStreamReader ipsr=new InputStreamReader(ips);
 	BufferedReader br=new BufferedReader(ipsr);
 	String ligne;
+	
 	int numligne=0;
 	while ((ligne=br.readLine())!=null){numligne++;
 		System.out.println(ligne);
+		if(numligne==1){l1=ligne;}
+		if(numligne==2){l2=ligne;}
+		if(numligne==3){l3=ligne;}
 		if(numligne>3){
 		carac.addElement(Integer.valueOf(ligne));}
 	}
@@ -47,18 +53,24 @@ for(int a=0;a<256;a++){
 		r=histo.get(a);
 		System.out.println("Nombre de pixels de valeur" +a+": "+r);
 }
-/*
+
 //création ou ajout dans le fichier texte
 try {
-	FileWriter fw = new FileWriter (fichier);
+	String fichier2="histogramme.pgm";
+	String chaine="";
+	FileWriter fw = new FileWriter (fichier2);
 	BufferedWriter bw = new BufferedWriter (fw);
-	PrintWriter fichierSortie = new PrintWriter (bw); 
+	PrintWriter fichierSortie = new PrintWriter (bw);
+	chaine=l1+"\n"+l2+"\n"+l3+"\n";
+	for(int s=0; s<histo.size();s++){
+		chaine+=histo.get(s)+"\n";
+	}
 		fichierSortie.println (chaine+"\n test de lecture et écriture !!"); 
 	fichierSortie.close();
-	System.out.println("Le fichier " + fichier + " a été créé!"); 
+	System.out.println("Le fichier " + fichier2 + " a été créé!"); 
 }
 catch (Exception e){
 	System.out.println(e.toString());
-}	*/	
+}	
 	}
 }
